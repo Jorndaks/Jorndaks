c75a531d33113e1c8e119d3a9159f29e416dc12f
dylan jordan <163300608+Jorndaks@users.noreply.github.com>
2024-03-14 11:53:19 +0000

    Update file1.py

--- file1.py
+++ file1.py
@@ -29,3 +29,23 @@ def compare(user_score, computer_score):
         return: "You win!"
     else:
         return "You Lose!"
+def play_game():
+    """Play a game of blackjack"""
+    user_cards = []
+    computer_cards = []
+    is_game_over = false
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        print(f"Your cards: [user _cards}, current score: {user_score}")
+        print(f"computer's first card: {computer_cards[0]}")
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+            else:
+                should_continue = input("Type 'y' to get another card, or 'n' to pass:")
+                if should_continue == 'y':
+                    user_cards.append(deal_card())
+                else: is_game_over = True
